25f91f433f93a97e9a68c1c8491c1ef81dbc4dae
Patrick Nguyen <superpatricko@gmail.com>
2015-09-26 13:26:40 -0400

    Add First React Component
    Had to create a new javascript file called homePage.js and wrote some
    JSX. main.js 'requires' the new HomePage links it to the index.html
    "app" class.

---
+++ src/components/homePage.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function() {
+		// whatever the render function returns is what's going to be displayed on the screen
+		// div uses className because class is a reserved word in JS
+		// when it gets converted into JS, it'll be called class
+		return (
+			<div className="jumbotron">
+				<h1>Pluralsight Administration</h1>
+				<p>React, React Router, and Flux for ultra responsive web apps.</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Home;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 $ = jQuery = require('jquery'); // two different ways to reference jQuery. 
 								// Bootstrap expects jQuery to be in the global namespace
+var React = require('react');
+var Home = require('./components/homePage');
 
-var App = console.log('Hello world from Browserify');
-
-module.exports = App;
+React.render(<Home />, document.getElementById('app'));
