b5a534ddd410dae0fd78051d4b71bfb07889d3b3
Patrick Nguyen <superpatricko@gmail.com>
2015-09-26 13:39:47 -0400

    Add Simple Routing
    - Add AboutPage React Component
    - Create an if statement in main.js so the linter doesn't complain about
      "use strict". We can't define "use strict" at the top of main.js
      because we have a global variable jQuery which Bootstrap needs

--- src/main.js
+++ src/main.js
@@ -2,5 +2,33 @@ $ = jQuery = require('jquery'); // two different ways to reference jQuery.
 								// Bootstrap expects jQuery to be in the global namespace
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+(function(win) {
+	"use strict";
 
-React.render(<Home />, document.getElementById('app'));
+	var App = React.createClass({
+		render: function() {
+			var Child;
+
+			// look at the route properties
+			switch(this.props.route) {
+				case 'about': Child = About; break;
+				default: Child = Home;
+			}
+
+			return (
+				<div>
+					<Child/>
+				</div>
+			);
+		}
+	});
+
+	function render() {
+		var route = win.location.hash.substr(1);	
+		React.render(<App route={route} />, document.getElementById('app'));
+	}
+
+	win.addEventListener('hashchange', render);
+	render();
+})(window);
