a4db7b5de621c812bbbd27dfd70ddee1a615cd73
Patrick Nguyen <superpatricko@gmail.com>
2015-09-26 20:59:47 -0400

    Add manageAuthorPage
    Create the necessary Links and Routes to navigate to the new page

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = require('react-router').Link;
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
@@ -23,6 +25,7 @@ var AuthorPage = React.createClass({
 			// Which is why we often wrap everything around a single div
 			<div>
 				<h1>Authors</h1>
+				<Link to="addAuthor" className="btn btn-default">Add Author</Link>
 				<AuthorList authors={this.state.authors} />
 			</div>
 		);
