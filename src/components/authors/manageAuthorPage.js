8547e7e6d4b92794831197d7333dc464948c9e7c
Patrick Nguyen <superpatricko@gmail.com>
2015-09-26 21:48:06 -0400

    Add Notifications using npm module toastr
    toastr allows for an easy way to handle quick notifications to the user
    such as a successful save or whatever

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,6 +4,7 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 // The setAuthorState(event) is called for every keystroke
 var ManageAuthorPage = React.createClass({
@@ -27,6 +28,7 @@ var ManageAuthorPage = React.createClass({
 	saveAuthor: function(event) {
 		event.preventDefault();
 		AuthorApi.saveAuthor(this.state.author);
+		toastr.success('Author saved.');
 		this.transitionTo('authors');
 	},
 
