3e68ff60425040eeb95ee9cf5209de65fb641847
Patrick Nguyen <superpatricko@gmail.com>
2015-10-01 13:19:41 -0400

    Add Stores (Change Listeners)
    - Need to add three core functions:
      - addChangeListener()
      - removeChangeListener()
      - emitChange()
    - Add npm module object-assign while you're at it

---
+++ src/stores/authorStore.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change'
+
+var AuthorStore = assign({}, EventEmitter.prototype, {
+	// whatever we put in here will have EventEmitter.prototype functionality
+	addChangeListener: function(callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
+
+	removeChangeListener: function(callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	},
+
+	emitChange: function() {
+		this.emit(CHANGE_EVENT);
+	}
+})
